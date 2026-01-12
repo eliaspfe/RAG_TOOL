827ff37a68fb825cb06bf67132b679923cf84ffd
eliaspfe <elias.pfeiffer@gmx.de>
2026-01-12 16:00:38 +0100

    scroll working fine now

--- frontend/src/components/InputBar.tsx
+++
@@ -1,8 +0,0 @@
-export default function InputBar() {
-  return (
-    <div className="input-bar">
-      <input type="text" placeholder="Frage eingeben..." />
-      <button>Senden</button>
-    </div>
-  );
-}
